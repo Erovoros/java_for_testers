3d6676630b456c12c4dedc0192ccba51a58e536f
Erovoros <erovoros@gmail.com>
2024-02-24 19:37:36 +0300

    Лекция 3.4. Двухуровневая архитектура тестов, классы-помощники

--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -3,24 +3,22 @@ package tests;
 import model.GroupData;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
+
 
 public class GroupRemovalTests extends TestBase{
 
-    private Map<String, Object> vars;
+
 
 
 
     @Test
     public void canRemoveGroup() {
-        app.openGroupsPage();
 
-
-        if (!app.isGroupPresent()) {
-            app.createGroup(new GroupData("group name", "group header", "group footer"));
+        if (!app.groups().isGroupPresent()) {
+            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
 
         }
-        app.removeGroup();
+        app.groups().removeGroup();
 
     }
 
