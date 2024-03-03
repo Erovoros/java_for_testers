8dcda5de6379373a5923ba9d947f3cc5fab1b725
Erovoros <erovoros@gmail.com>
2024-03-03 21:48:04 +0300

    Лекция 4.5. Параметризованные тесты

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -3,55 +3,68 @@ package tests;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class GroupCreationTests extends TestBase{
 
 
-    @Test
-    public void canCreateGroup() {
-        int groupCount = app.groups().getCount();
-
-        app.groups().createGroup(new GroupData("some name", "some header", "some footer"));
-        int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount +1, newGroupCount);
-
-
-    }
-
-    @Test
-    public void canCreateGroupWithEmptyName() {
-
-        app.groups().createGroup(new GroupData());
-
-    }
-
-    @Test
-    public void canCreateGroupWithNameOnly() {
-        app.groups().createGroup(new GroupData().withName("some name"));
-    }
-    @Test
-    public void canCreateGroupWithHeaderOnly() {
-        app.groups().createGroup(new GroupData().withHeader("some header"));
-    }
-
-
-    @Test
-    public void canCreateGroupWithFooterOnly() {
-        app.groups().createGroup(new GroupData().withFooter("some footer"));
-    }
-
-    @Test
-    public void canCreateMultipleGroups() {
-        int n = 5;
-        int groupCount = app.groups().getCount();
-
-        for (int i = 0; i < n; i ++){
-            app.groups().createGroup(new GroupData(randomString(i *10), "some header", "some footer"));
+    public static List<GroupData> groupProvider() {
+        var result = new ArrayList<GroupData>();                ;
+        for (var name: List.of("", "group name")) {
+            for (var header : List.of("", "group header")){
+                for (var footer: List.of("", "group footer")) {
+                    result.add(new GroupData(name, header, footer));
+                }
+            }
         }
+        for (int i = 0; i < 5; i++){
+            result.add(new GroupData (randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
+    }
 
 
+
+
+
+    @ParameterizedTest
+    @MethodSource("groupProvider")
+    public void canCreateMultipleGroups(GroupData group) {
+
+        int groupCount = app.groups().getCount();
+        app.groups().createGroup(group);
         int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount + n, newGroupCount);
+        Assertions.assertEquals(groupCount + 1, newGroupCount);
+
+
+
+
+    }
+
+
+    public static List<GroupData> negativeGroupProvider() {
+        var result = new ArrayList<GroupData>(List.of(
+                new GroupData ("group name'", "", "")));
+
+        return result;
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("negativeGroupProvider")
+    public void canNotCreateGroup(GroupData group) {
+
+        int groupCount = app.groups().getCount();
+        app.groups().createGroup(group);
+        int newGroupCount = app.groups().getCount();
+        Assertions.assertEquals(groupCount, newGroupCount);
 
 
     }
