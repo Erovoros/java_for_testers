970cac3395c0112a5534056d1c578bfad2a1598f
Erovoros <erovoros@gmail.com>
2024-03-10 01:48:32 +0300

    Лекция 5.3. Генератор тестовых данных, анализ опций командной строки

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,10 @@
-package tests;
+package ru.stqa.addressbook.tests;
 
-import model.ContactData;
-import model.GroupData;
-import org.junit.jupiter.api.Assertions;
+import ru.stqa.addressbook.common.CommonFunctions;
+import ru.stqa.addressbook.model.ContactData;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase{
@@ -18,8 +14,8 @@ public class ContactCreationTests extends TestBase{
     public void canCreateContact() {
 
         var contact = new ContactData()
-                .withFirstName(randomString(10))
-                .withLastName(randomString(10))
+                .withFirstName(CommonFunctions.randomString(10))
+                .withLastName(CommonFunctions.randomString(10))
                 .withPhoto(randomFile("src/test/resources/images/"));
         app.contacts().createContact(contact);
 
@@ -51,11 +47,11 @@ public class ContactCreationTests extends TestBase{
         }
         for (int i = 0; i < 5; i++){
             result.add(new ContactData()
-                    .withFirstName(randomString(i * 10))
-                    .withLastName(randomString(i * 10))
-                    .withAddress(randomString(i * 10))
-                    .withEmail(randomString(i * 10))
-                    .withMobile(randomString(i * 10)));
+                    .withFirstName(CommonFunctions.randomString(i * 10))
+                    .withLastName(CommonFunctions.randomString(i * 10))
+                    .withAddress(CommonFunctions.randomString(i * 10))
+                    .withEmail(CommonFunctions.randomString(i * 10))
+                    .withMobile(CommonFunctions.randomString(i * 10)));
 
         }
         return result;
--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -1,6 +1,6 @@
-package tests;
+package ru.stqa.addressbook.tests;
 
-import manager.ApplicationManager;
+import ru.stqa.addressbook.manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
@@ -20,17 +20,6 @@ public class TestBase {
 
     }
 
-
-    public static String randomString(int n) {
-        var rnd = new Random();
-        var result = "";
-        for (int i = 0; i < n; i++) {
-            result = result + (char)('a' +rnd.nextInt(26));
-        }
-
-
-        return result;
-    }
 
     public static String randomFile(String dir) {
         var fileNames = new File(dir).list();
